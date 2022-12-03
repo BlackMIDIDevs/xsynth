6a0353403bc13665dec0cd135062e6058a24b61d
MBMS <31241793+MyBlackMIDIScore@users.noreply.github.com>
2022-12-03 09:33:55 +0000

    More SFZ parser fixes (#20)
    - Fix `default_path` parsing
    - Ignore regions with samples that don't exist

--- soundfonts/src/sfz/mod.rs
+++ soundfonts/src/sfz/mod.rs
@@ -110,7 +110,11 @@ impl RegionParamsBuilder {
             self.sample?.into()
         };
 
-        let sample_path = base_path.join(relative_sample_path);
+        let mut sample_path = base_path.join(relative_sample_path);
+        match sample_path.canonicalize() {
+            Ok(path) => sample_path = path,
+            Err(_) => return None,
+        }
 
         let keyrange: RangeInclusive<u8>;
 
