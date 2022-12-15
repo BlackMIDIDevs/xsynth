a9b881c52d522dcf6d147e34fdba341686c912b5
arduano <leonid.shchurov@gmail.com>
2022-12-15 13:46:32 +1100

    Updated cutoff to use an enum

--- core/src/effects/cutoff.rs
+++ core/src/effects/cutoff.rs
@@ -1,91 +1,144 @@
 use simdeez::Simd;
 use soundfonts::FilterType;
 
-pub struct SingleChannelFilter {
-    filter_type: FilterType,
-    previous: Vec<f32>,
-    previous_unedited: Vec<f32>,
+pub struct LowpassFilterBase {
     alpha: f32,
     sample_rate: f32,
+    previous: Vec<f32>,
 }
 
-impl SingleChannelFilter {
-    pub fn new(filter_type: FilterType, cutoff: f32, sample_rate: f32) -> Self {
-        let alpha = Self::calculate_alpha(filter_type, cutoff, sample_rate);
-
-        let passes = match filter_type {
-            FilterType::LowPass { passes } => passes,
-            FilterType::HighPass { passes } => passes,
-        };
+impl LowpassFilterBase {
+    pub fn new(passes: usize, cutoff: f32, sample_rate: f32) -> Self {
+        let alpha = Self::calculate_alpha(cutoff, sample_rate);
 
         Self {
-            filter_type,
             previous: vec![0.0; passes],
-            previous_unedited: vec![0.0; passes],
             alpha,
             sample_rate,
         }
     }
 
     pub fn set_cutoff(&mut self, cutoff: f32) {
-        self.alpha = Self::calculate_alpha(self.filter_type, cutoff, self.sample_rate);
+        self.alpha = Self::calculate_alpha(cutoff, self.sample_rate);
     }
 
-    fn calculate_alpha(filter_type: FilterType, cutoff: f32, sample_rate: f32) -> f32 {
+    fn calculate_alpha(cutoff: f32, sample_rate: f32) -> f32 {
         let rc = 1.0 / (cutoff * 2.0 * core::f32::consts::PI);
         let dt = 1.0 / sample_rate;
-
-        match filter_type {
-            FilterType::LowPass { .. } => dt / (rc + dt),
-            FilterType::HighPass { .. } => rc / (rc + dt),
-        }
+        dt / (rc + dt)
     }
 
     pub fn process_sample(&mut self, val: f32) -> f32 {
         let mut out = val;
-        match self.filter_type {
-            FilterType::LowPass { passes } => {
-                for pass in 0..passes {
-                    out = self.alpha * out + (1.0 - self.alpha) * self.previous[pass];
-                    self.previous[pass] = out;
-                }
-            }
-            FilterType::HighPass { passes } => {
-                for pass in 0..passes {
-                    let previous = out;
-                    out = self.alpha * (self.previous[pass] + out - self.previous_unedited[pass]);
-                    self.previous[pass] = out;
-                    self.previous_unedited[pass] = previous;
-                }
-            }
+        for pass in 0..self.previous.len() {
+            out = self.alpha * out + (1.0 - self.alpha) * self.previous[pass];
+            self.previous[pass] = out;
         }
         out
     }
 
     pub fn process_sample_simd<S: Simd>(&mut self, val: S::Vf32) -> S::Vf32 {
         let mut out = val;
-        match self.filter_type {
-            FilterType::LowPass { passes } => {
-                for pass in 0..passes {
-                    for i in 0..S::VF32_WIDTH {
-                        out[i] = self.alpha * out[i] + (1.0 - self.alpha) * self.previous[pass];
-                        self.previous[pass] = out[i];
-                    }
-                }
-            }
-            FilterType::HighPass { passes } => {
-                for pass in 0..passes {
-                    for i in 0..S::VF32_WIDTH {
-                        let previous = out[i];
-                        out[i] = self.alpha
-                            * (self.previous[pass] + out[i] - self.previous_unedited[pass]);
-                        self.previous[pass] = out[i];
-                        self.previous_unedited[pass] = previous;
-                    }
-                }
+        for pass in 0..self.previous.len() {
+            for i in 0..S::VF32_WIDTH {
+                out[i] = self.alpha * out[i] + (1.0 - self.alpha) * self.previous[pass];
+                self.previous[pass] = out[i];
             }
         }
         out
+    }
+}
+
+pub struct HighpassFilterBase {
+    alpha: f32,
+    sample_rate: f32,
+    previous: Vec<f32>,
+    previous_unedited: Vec<f32>,
+}
+
+impl HighpassFilterBase {
+    pub fn new(passes: usize, cutoff: f32, sample_rate: f32) -> Self {
+        let alpha = Self::calculate_alpha(cutoff, sample_rate);
+
+        Self {
+            previous_unedited: vec![0.0; passes],
+            previous: vec![0.0; passes],
+            alpha,
+            sample_rate,
+        }
+    }
+
+    pub fn set_cutoff(&mut self, cutoff: f32) {
+        self.alpha = Self::calculate_alpha(cutoff, self.sample_rate);
+    }
+
+    fn calculate_alpha(cutoff: f32, sample_rate: f32) -> f32 {
+        let rc = 1.0 / (cutoff * 2.0 * core::f32::consts::PI);
+        let dt = 1.0 / sample_rate;
+        rc / (rc + dt)
+    }
+
+    pub fn process_sample(&mut self, val: f32) -> f32 {
+        let mut out = val;
+        for pass in 0..self.previous.len() {
+            let previous = out;
+            out = self.alpha * (self.previous[pass] + out - self.previous_unedited[pass]);
+            self.previous[pass] = out;
+            self.previous_unedited[pass] = previous;
+        }
+        out
+    }
+
+    pub fn process_sample_simd<S: Simd>(&mut self, val: S::Vf32) -> S::Vf32 {
+        let mut out = val;
+        for pass in 0..self.previous.len() {
+            for i in 0..S::VF32_WIDTH {
+                let previous = out[i];
+                out[i] = self.alpha * (self.previous[pass] + out[i] - self.previous_unedited[pass]);
+                self.previous[pass] = out[i];
+                self.previous_unedited[pass] = previous;
+            }
+        }
+        out
+    }
+}
+
+pub enum SingleChannelFilter {
+    LowPass(LowpassFilterBase),
+    HighPass(HighpassFilterBase),
+}
+
+impl SingleChannelFilter {
+    pub fn new(filter_type: FilterType, cutoff: f32, sample_rate: f32) -> Self {
+        match filter_type {
+            FilterType::LowPass { passes } => {
+                Self::LowPass(LowpassFilterBase::new(passes, cutoff, sample_rate))
+            }
+            FilterType::HighPass { passes } => {
+                Self::HighPass(HighpassFilterBase::new(passes, cutoff, sample_rate))
+            }
+        }
+    }
+
+    pub fn set_cutoff(&mut self, cutoff: f32) {
+        match self {
+            Self::LowPass(filter) => filter.set_cutoff(cutoff),
+            Self::HighPass(filter) => filter.set_cutoff(cutoff),
+        }
+    }
+
+    pub fn process_sample(&mut self, val: f32) -> f32 {
+        match self {
+            Self::LowPass(filter) => filter.process_sample(val),
+            Self::HighPass(filter) => filter.process_sample(val),
+        }
+    }
+
+    pub fn process_sample_simd<S: Simd>(&mut self, val: S::Vf32) -> S::Vf32 {
+        match self {
+            Self::LowPass(filter) => filter.process_sample_simd::<S>(val),
+            Self::HighPass(filter) => filter.process_sample_simd::<S>(val),
+        }
     }
 }
 
